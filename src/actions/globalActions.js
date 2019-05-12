330a5cf09f7566c3855b7471589923d1e262e5a7
Julian Hernandez <julian1@cimarron.me>
2019-05-11 17:51:59 -0700

    global store

---
+++ src/actions/globalActions.js
@@ -0,0 +1,4 @@
+import { createAction } from 'redux-starter-kit'
+
+
+export const setActiveCell = createAction('SET_ACTIVE_CELL')
---
+++ src/reducers/globalReducer.js
@@ -0,0 +1,13 @@
+import { createReducer } from 'redux-starter-kit'
+
+export const INITIAL_STATE = {
+  activeCell: null,
+  rows: 3,
+  columns: 3,
+}
+
+export default createReducer(INITIAL_STATE, {
+  'SET_ACTIVE_CELL': (state, { payload }) => {
+    state.activeCell = payload
+  },
+})
