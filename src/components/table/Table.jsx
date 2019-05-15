8a972d24c11fac1ba47fb6304be9593eb3ad455d
Julian Hernandez <julian1@cimarron.me>
2019-05-15 02:35:12 -0700

    Table component

--- src/components/table/Table.jsx
+++ src/components/table/Table.jsx
@@ -2,81 +2,85 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import styled from '@emotion/styled'
 import { connect } from 'react-redux'
+import { interpret } from 'xstate'
 
-import Cell from '../cell/Cell'
+import CellData from '../cell/CellData'
+import { getColumnNames } from '~/library/utils'
+import getKeyboardNavMachine from './keyboardNavMachine'
 
 
 const RootTable = styled.table`
-  border: 2px solid lightgray;
-  border-collapse: collapse;
-  border-spacing: 0px;
-
-  td {
-    padding: 0;
-    border: 1px dotted #bbb;
-  }
+  border-top: 2px solid ${props => props.theme.colors.cell.border};
+  border-left: 2px solid ${props => props.theme.colors.cell.border};
+  border-right: 1px solid ${props => props.theme.colors.cell.border};
+  border-bottom: 1px solid ${props => props.theme.colors.cell.border};
 `
 
-class Table extends React.PureComponent {
+const Td = styled.td`
+  width: 130px;
+  height: 26px;
+`
+
+export class Table extends React.PureComponent {
   static propTypes = {
     activeCell: PropTypes.string,
-    rows: PropTypes.number,
-    columns: PropTypes.number,
+    rows: PropTypes.number.isRequired,
+    columns: PropTypes.number.isRequired,
   }
 
-  handleTableOnClick = (event) => {
+  constructor(props) {
+    super(props)
+    this.handleTableOnClick = this.handleTableOnClick.bind(this)
+    this.handleTableOnKeyDown = this.handleTableOnKeyDown.bind(this)
+    this.columnNames = getColumnNames(props.columns)
+  }
+
+  keyboardNavMachine = getKeyboardNavMachine(this.props.rows, this.props.columns)
+  focusService = interpret(this.keyboardNavMachine)
+
+  componentDidMount() {
+    this.focusService.start()
+  }
+
+  componentWillUnmount() {
+    this.focusService.stop()
+  }
+
+  handleTableOnClick(event) {
     event.stopPropagation()
   }
 
-  handleTableOnKeyDown = (event) => {
+  handleTableOnKeyDown(event) {
     event.stopPropagation()
-    this.moveFocus(event.key, event.target.dataset.location)
+    this.moveFocus(event.key, event.target.id)
   }
 
   moveFocus(key, location) {
-    const { rows, columns } = this.props
-    const currRow = location.codePointAt(0) - 'a'.codePointAt(0)
-    const currCol = Number.parseInt(location[1]) - 1
-    let nextRow = currRow
-    let nextCol = currCol
-
-    if ('ArrowUp' === key) { nextRow = currRow - 1 }
-    else if ('ArrowRight' === key) { nextCol = currCol + 1 }
-    else if (['ArrowDown', 'Enter'].includes(key)) { nextRow = currRow + 1 }
-    else if ('ArrowLeft' === key) { nextCol = currCol - 1 }
-    else { return }
-
-    if (Math.min(nextRow, nextCol) >= 0 && nextRow < rows && nextCol < columns) {
-      nextRow = String.fromCodePoint('a'.codePointAt(0) + nextRow)
-      nextCol = nextCol + 1
-      document.querySelector(`#${nextRow}${nextCol}`).focus()
-    }
+    this.focusService.send({ type: 'MOVE_FOCUS', key, location })
   }
 
-  renderCells(rowLocation) {
+  renderDataCells(rowNumber) {
     return new Array(this.props.columns).fill(0).map((_, i) => {
-      const location = rowLocation + (i+1)
+      const location = `${this.columnNames[i]}-${rowNumber}`
 
       return (
-        <td
+        <Td
           key={location}
         >
-          <Cell
+          <CellData
             isActive={this.props.activeCell === location}
             location={location}
           />
-        </td>
+        </Td>
       )
     })
   }
 
   renderRows() {
     return new Array(this.props.rows).fill(0).map((_, i) => {
-      const rowLocation = String.fromCharCode('a'.charCodeAt(0) + i)
-
       return (
         <tr key={i}>
-          {this.renderCells(rowLocation)}
+          {this.renderDataCells(i + 1)}
         </tr>
       )
     })
@@ -96,8 +100,8 @@ class Table extends React.PureComponent {
   }
 }
 
-const mapStateToProps = (props) => {
-  const { activeCell, rows, columns } = props
+const mapStateToProps = (state) => {
+  const { activeCell, rows, columns } = state.global
   return { activeCell, rows, columns }
 }
 
