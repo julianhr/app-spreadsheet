deea0856ad45b290142bd630fa5ce5e7bb9378cd
Julian Hernandez <julian1@cimarron.me>
2019-06-06 01:46:14 -0700

    update redux actions and reducers for graph solver

--- src/actions/__tests__/tableActions.test.js
+++ src/actions/__tests__/tableActions.test.js
@@ -1,39 +1,35 @@
-import appStoreGen from '~/reducers/'
+import { appStoreGen } from '~/reducers/'
 import * as actions from '~/actions/tableActions'
 
 
 describe('tableActions', () => {
   describe('#setCellData', () => {
     it('sets currently active cell', async () => {
-      const cell = 'B-2'
-      const location = cell
-      const entered = 5
-      const isEnteredValid = true
+      const location = 'B-2'
+      const entered = '5'
       const result = 5
       const appStore = appStoreGen()
 
-      expect(appStore.getState().table[cell]).toBeUndefined()
-      await appStore.dispatch(actions.setCellData(location, entered, isEnteredValid, result))
-      expect(appStore.getState().table[cell]).toEqual({ entered, isEnteredValid, result })
+      expect(appStore.getState().table[location]).toBeUndefined()
+      await appStore.dispatch(actions.setCellData(location, entered))
+      expect(appStore.getState().table[location]).toEqual({ entered, result })
     })
   })
 
   describe('#clearCellData', () => {
     it('deletes value from store', async () => {
-      const cell = 'B-2'
-      const location = cell
-      const entered = 5
-      const isEnteredValid = true
+      const location = 'B-2'
+      const entered = '5'
       const result = 5
       const appStore = appStoreGen()
 
-      expect(appStore.getState().table[cell]).toBeUndefined()
+      expect(appStore.getState().table[location]).toBeUndefined()
 
-      appStore.dispatch(actions.setCellData(location, entered, isEnteredValid, result))
-      expect(appStore.getState().table[cell]).toEqual({ entered, isEnteredValid, result })
+      appStore.dispatch(actions.setCellData(location, entered))
+      expect(appStore.getState().table[location]).toEqual({ entered, result })
 
-      await appStore.dispatch(actions.clearCellData(cell))
-      expect(appStore.getState().table[cell]).toBeUndefined()
+      await appStore.dispatch(actions.clearCellData(location))
+      expect(appStore.getState().table[location]).toBeUndefined()
     })
   })
 })
--- src/actions/tableActions.js
+++ src/actions/tableActions.js
@@ -1,14 +1,19 @@
-export function setCellData(location, entered, isEnteredValid, result) {
+import graph from '~/formulas/graph'
+
+
+export function setCellData(location, entered) {
+  const result = graph.interpret(location, entered)
+
   return {
-    type: 'SET_CELL_VALUE',
-    payload: {
-      location,
-      data: {
-        entered,
-        result,
-        isEnteredValid,
-      }
-    }
+    type: 'SET_CELL_DATA',
+    payload: { location, data: { entered, result } },
+  }
+}
+
+export function replaceCellData(location, entered, result) {
+  return {
+    type: 'SET_CELL_DATA',
+    payload: { location, data: { entered, result } },
   }
 }
 
@@ -16,13 +21,16 @@ export function clearCellData(location) {
   return (dispatch, getState) => {
     const value = getState().table[location]
 
-    if (value === undefined) { return Promise.resolve() }
+    graph.delVertex(location)
 
-    let action = {
-      type: 'DELETE_CELL_DATA',
-      payload: location,
+    if (value === undefined) {
+      return Promise.resolve()
     }
 
-    return Promise.resolve(dispatch(action))
+
+    return Promise.resolve(dispatch({
+      type: 'DELETE_CELL_DATA',
+      payload: location,
+    }))
   }
 }
