a60f3e3590e9d2f5905711c0b70cfbe096a33c9b
Julian Hernandez <julian1@cimarron.me>
2019-06-09 20:47:02 -0700

    update formulas object to hold metadata

--- src/formulas/formulaFunctions.js
+++ src/formulas/formulaFunctions.js
@@ -1,77 +1,120 @@
-const fn = {
-  AVERAGE: function(...args) {
-    if (args.length === 0) {
-      throw new Error('Empty elements')
-    }
-
-    return this.SUM(...args) / this.COUNT(...args)
-  },
-
-  CONCAT: function(...args) {
-    if (args.length === 0) {
-      throw new Error('Empty elements')
-    }
-
-    return args.reduce((prev, curr) => (prev + curr), '')
-  },
-
-  COUNT: (...args) => args.length,
-
-  MAX: function(...args) {
-    if (args.length === 0) {
-      throw new Error('Empty elements')
-    }
-
-    args.forEach(arg => {
-      if (typeof arg !== 'number') {
-        throw new TypeError(`${arg} is not a number`)
+const Fn = {
+  AVERAGE: {
+    fn: (...args) => {
+      if (args.length === 0) {
+        throw new Error('Empty elements')
       }
-    })
-
-    return Math.max(...args)
-  },
-
-  MIN: function(...args) {
-    if (args.length === 0) {
-      throw new Error('Empty elements')
-    }
-
-    args.forEach(arg => {
-      if (typeof arg !== 'number') {
-        throw new TypeError(`${arg} is not a number`)
-      }
-    })
-
-    return Math.min(...args)
-  },
-
-  POWER: function(...args) {
-    if (args.length !== 2) {
-      throw new Error('Formula takes only two elements')
-    }
   
-    const [base, exp] = args
-    return Math.pow(base, exp)
+      return Fn.SUM.fn(...args) / Fn.COUNT.fn(...args)
+    },
+    definition: 'AVERAGE(value1, value2, ...)',
+    example: 'AVERAGE(A1:B3, 3)',
+    summary: 'Returns the average from a series of numbers.',
   },
-
-  SQRT: function(...args) {
-    if (args.length !== 1) {
-      throw new Error(`Formula takes only one element`)
-    }
-
-    return Math.sqrt(args[0])
+  CONCAT: {
+    fn: (...args) => {
+      if (args.length === 0) {
+        throw new Error('Empty elements')
+      }
+  
+      return args.reduce((prev, curr) => (prev + curr), '')
+    },
+    definition: 'CONCAT("text1", "text2", ...)',
+    example: 'CONCAT("one", "two")',
+    summary: 'Returns a single concatenated string from a series of strings.',
   },
-
-  SUM: function(...args) {
-    if (args.length === 0) {
-      throw new Error('Empty elements')
-    }
-
-    return args.reduce((prev, curr) => prev + curr)
+  COUNT: {
+    fn: (...args) => args.length,
+    definition: 'COUNT(element1, element2, ...)',
+    example: 'COUNT(A1:B3, 2, C4)',
+    summary: 'Returns the number of non-empty elements in a set.',
   },
+  MAX: {
+    fn: (...args) => {
+      if (args.length === 0) {
+        throw new Error('Empty elements')
+      }
+  
+      args.forEach(arg => {
+        if (typeof arg !== 'number') {
+          throw new TypeError(`${arg} is not a number`)
+        }
+      })
+  
+      return Math.max(...args)
+    },
+    definition: 'MAX(value1, value2, ...)',
+    example: 'MAX(A1:B3, 4, C3)',
+    summary: 'Returns the maximum value from a series of numbers',
+  },
+  MIN: {
+    fn: (...args) => {
+      if (args.length === 0) {
+        throw new Error('Empty elements')
+      }
+  
+      args.forEach(arg => {
+        if (typeof arg !== 'number') {
+          throw new TypeError(`${arg} is not a number`)
+        }
+      })
+  
+      return Math.min(...args)
+    },
+    definition: 'MIN(value1, value2, ...)',
+    example: 'MIN(A1:B3, 4, C3)',
+    summary: 'Returns the minimum value from a series of numbers.',
+  },
+  POWER: {
+    fn: (...args) => {
+      if (args.length !== 2) {
+        throw new Error('Formula takes only two elements')
+      }
+    
+      const [base, exp] = args
+      return Math.pow(base, exp)
+    },
+    definition: 'POWER(base, exponent)',
+    example: 'POWER(A3,2)',
+    summary: 'Returns a number raised to an exponent.'
+  },
+  SQRT: {
+    fn: (...args) => {
+      if (args.length !== 1) {
+        throw new Error(`Formula takes only one element`)
+      }
+  
+      return Math.sqrt(args[0])
+    },
+    definition: 'SQRT(value)',
+    example: 'SQRT(9) or SQRT(A3)',
+    summary: 'Returns the square root of a number.'
+  },
+  SUM: {
+    fn: (...args) => {
+      if (args.length === 0) {
+        throw new Error('Empty elements')
+      }
+  
+      return args.reduce((prev, curr) => prev + curr)
+    },
+    definition: 'SUM(value1, value2, ...)',
+    example: 'SUM(A1:B3, 3, C5)',
+    summary: 'Retuns the sum from a series of numbers.',
+  }
 }
 
 // aliases
-fn.POW = fn.POWER
+Fn.AVG = {
+  ...Fn.AVERAGE,
+  definition: 'AVG(value1, value2, ...)',
+  example: 'AVG(A1:B3, 3)',
+}
 
-export default fn
+Fn.POW = {
+  ...Fn.POWER,
+  definition: 'POW(base, exponent)',
+  example: 'POW(A3,2)',
+}
+
+export default Fn
